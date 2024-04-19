57c3300aaf8f3d70f84b359e84ce1e5cfeef6b7c
Arbaaz Khan <arbaazkhanark23@gmail.com>
2024-04-19 23:21:28 +0530

    try2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const cors=require("cors");
 const dataConn = require("./database");
 const postRouter=require("./router/postRouter");
+const path=require("path");
 
 
 const app=express();
